244c2c99891e95b7929557951356225f30021d03
Simon Vandel Sillesen <simon.vandel@gmail.com>
2015-10-13 15:39:20 +0200

    Lille update til spellchecking

--- Report/spellcheck.sh
+++ Report/spellcheck.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 spellcheck() {
-  echo ------- Spellchecking $1 ----------
-  #cat $1 | aspell -a --per-conf=./aspell.conf | grep -v -E '\*|International Ispell Version' | uniq
-  aspell -c $1 --per-conf=./aspell.conf # | grep -v -E '\*|International Ispell Version' | uniq
+  echo Spellchecking $1
+  aspell -c $1 --per-conf=./aspell.conf
 }
 
 export -f spellcheck
