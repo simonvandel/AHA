d43f4f3f1c78e06e9b991dc9f9c7574254be979e
KaFuCh <kfch13@student.aau.dk>
2015-12-01 14:39:31 +0100

    cleanup in Reasoner

--- AHA/src/main/java/Reasoner/Reasoner.java
+++ AHA/src/main/java/Reasoner/Reasoner.java
@@ -124,14 +124,3 @@ interface IModel {
   Action CalculateAction(Sample s); //should calculate the most likely action to occur (which is above a certain threshold)
   void TakeFeedback(Action a1, Action a2); //used to update the reasoners model based on two wrong actions
 }
-
-//interface ICom {
-//  public void sendAction(Action act);
-//}
-
-//interface Action {
-//}
-//
-//class Sample {
-//  List<Action> Actions = null;
-//}
