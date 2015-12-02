86795efde3876648f8b401d2c841c38a843b4f68
KaFuCh <kfch13@student.aau.dk>
2015-12-02 16:21:58 +0100

    Sanitiser (part of sampler) first commit. Still need to update DB

--- AHA/src/main/java/Reasoner/Reasoner.java
+++ AHA/src/main/java/Reasoner/Reasoner.java
@@ -23,6 +23,7 @@ import java.util.concurrent.TimeUnit;
  * void TakeFeedback(Action a1, Action a2); //used to update the reasoners model based on two wrong actions
  */
 public class Reasoner {
+  private static Reasoner reasoner;
   private DB db = null;
   private IModel currentModel = null;
   private Communicator com = null;
@@ -31,15 +32,16 @@ public class Reasoner {
           .newBuilder()
           .expireAfterWrite(5, TimeUnit.SECONDS)
           .build();
-  //husk actions vi har modtaget, indenfor x sekunder, så vi kan tjekke fejl der går på tværs af samples
-  private Cache<String, Action> receivedActions = CacheBuilder
-          .newBuilder()
-          .expireAfterWrite(5, TimeUnit.SECONDS)
-          .build();
 
-  public Reasoner() {
+  private Reasoner() {
     //db = DB.getInstance();
     //currentModel = db.getModel();
+  }
+  public static Reasoner getInstance(){
+    if(reasoner == null){
+      reasoner = new Reasoner();
+    }
+    return reasoner;
   }
 
   /**
@@ -63,33 +65,8 @@ public class Reasoner {
    * @return an action which is probable according to the model
      */
   public Action reason(Sample sample) {
-    //region Update received actions cache
-    for (Action action: sample.getActions()){
-      receivedActions.put(action.toString(), action);
-    }
-    //endregion
-
-    //region Feedback
-    receivedActions.cleanUp();
-    sentActions.cleanUp();
-    List<Action> validActions = new ArrayList<Action>(receivedActions.asMap().values());
-    for (int i = 0; i < validActions.size() - 1; i++) {
-      for (int j = i + 1; j < validActions.size() - 1; j++) { //Get all combinations of actions in sample
-        if (validActions.get(i) == inverseAction(validActions.get(j))) { //If two actions are inverse to each other
-          validActions.remove(sample.getActions().get(i)); //remove invalid actions
-          validActions.remove(sample.getActions().get(j));
-          if(sentActions.getIfPresent(sample.getActions().get(i).toString()) != null){ //if first action was system action
-            //db.flagModel(sample.getActions().get(i), sample.getActions().get(i)); //flag the model in DB, so learner knows a mistake was made
-            currentModel.TakeFeedback(sample.getActions().get(i), sample.getActions().get(j)); //Update our current model, to not make the same mistake twice
-          }
-        }
-      }
-    }
-
-    //db.flagEntries(validActions);
-    //endregion
-
     Action action = currentModel.CalculateAction(sample);
+    sentActions.cleanUp();
     if(action != null){
       sentActions.put(action.toString(), action);
     }
@@ -97,12 +74,16 @@ public class Reasoner {
   }
 
   /**
-   * Gives the inverse action to the one given
-   * @param action the action from which the inverse is wanted
-     * @return the inverse action to the one given as input
-     */
-  private Action inverseAction(Action action) {
-    return new Action(action.getVal2(), action.getVal1(), action.getDevice());
+   * Given an action, determines if this was performed by the system within the last 5 seconds
+   * @param action the action to check for
+   * @return true if the action was performed by the system, false otherwise
+   */
+  public boolean wasSystemAction(Action action){
+    sentActions.cleanUp();
+    if(sentActions.asMap().containsValue(action)){
+      return true;
+    }
+    return false;
   }
 }
 
--- AHA/src/main/java/Sampler/Sampler.java
+++ AHA/src/main/java/Sampler/Sampler.java
@@ -2,10 +2,15 @@ package Sampler;
 
 import Normaliser.NormalizedSensorState;
 import Normaliser.NormalizedValue;
+import Reasoner.Reasoner;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.reflect.ClassPath;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Sampler {
@@ -14,6 +19,13 @@ public class Sampler {
   private NormalizedSensorState mPrevious;
   private int mScopeSize = 0;
   private int mEmulatableNum = 0;
+  //husk actions vi har fundet, indenfor 5 sekunder, så vi kan tjekke fejl der går på tværs af samples
+  private Cache<String, Action> receivedActions = CacheBuilder
+      .newBuilder()
+      .expireAfterWrite(5, TimeUnit.SECONDS)
+      .build();
+  private Reasoner reasoner = Reasoner.getInstance();
+
 
   List<NormalizedValue> zeroVal = new ArrayList<NormalizedValue>();
 
@@ -49,6 +61,7 @@ public class Sampler {
    */
   public Sample getSample(NormalizedSensorState newState) {
     List<Action> acs = findActions(mPrevious,newState);
+    findInvertedActionsAndCleanStates(acs, mPrevious, newState);
     moveScope(newState);
     return new Sample(mHistory,newState.getTime().getNano(),acs);
   }
@@ -80,5 +93,49 @@ public class Sampler {
                 .stream()
                 .filter(sensorValue -> sensorValue.isEmulatable())
                 .collect(Collectors.toList());
+  }
+
+  /**
+   * Gives the inverse action to the one given
+   * @param action the action from which the inverse is wanted
+   * @return the inverse action to the one given as input
+   */
+  private Action inverseAction(Action action) {
+    return new Action(action.getVal2(), action.getVal1(), action.getDevice());
+  }
+
+  private void findInvertedActionsAndCleanStates(List<Action> actions, NormalizedSensorState state1, NormalizedSensorState state2){
+    //region Update found actions cache
+    for (Action action: actions){
+      receivedActions.put(action.toString(), action);
+    }
+    //endregion
+
+    //region Identify and correct inverted actions
+    receivedActions.cleanUp();
+    List<Action> validActions = new ArrayList<Action>(receivedActions.asMap().values());
+    for (int i = 0; i < validActions.size() - 1; i++) {
+      for (int j = i + 1; j < validActions.size() - 1; j++) { //Get all combinations of actions in sample
+        if (validActions.get(i) == inverseAction(validActions.get(j))) { //If two actions are inverse to each other
+          state2
+              .getNormalizesValues()
+              .get(state2
+                  .getNormalizesValues()
+                  .indexOf(validActions
+                      .get(j)
+                      .getVal2()))
+              .setValue(validActions
+                  .get(j)
+                  .getVal1()
+                  .getValue()); //Correct the state to have the value of before the action happened
+
+          if(reasoner.wasSystemAction(validActions.get(i))){
+            //Flag the model in the DB
+            //Tell Reasoner to update its model??
+          }
+        }
+      }
+    }
+    //endregion
   }
 }
