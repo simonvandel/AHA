a9629240e57fee44dfd0f4bb72a02e4f962dd405
KaFuCh <kfch13@student.aau.dk>
2015-12-03 16:38:28 +0100

    Sampler now adds sanitised samples to DB

--- AHA/src/main/java/Sampler/Sampler.java
+++ AHA/src/main/java/Sampler/Sampler.java
@@ -1,11 +1,10 @@
 package Sampler;
 
+import Database.DB;
 import Normaliser.NormalizedSensorState;
 import Normaliser.NormalizedValue;
 import Reasoner.Reasoner;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.reflect.ClassPath;
+import com.google.common.cache.*;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -15,14 +14,31 @@ import java.util.stream.Collectors;
 
 public class Sampler {
   private static Sampler mSampler;
+  private DB db = DB.getInstance();
   private List<NormalizedSensorState> mHistory;
   private NormalizedSensorState mPrevious;
   private int mScopeSize = 0;
   private int mEmulatableNum = 0;
+  private RemovalListener<String, Sample> sanitizerListener = removalNotification -> {
+    Sample sample = removalNotification.getValue();
+    if(removalNotification.getCause() != RemovalCause.EXPIRED){
+      //we done goofed
+      //something was removed from uncleanSamples due to something else than time expiration
+      return;
+    }
+    if(sample == null){
+      //we're fucked
+      //value was garbage-collected before removalListener got to it. Yay dynamic garbage collection! just ignore? not much else to do..
+      return;
+    }
+    db.putStateScopeIntoDB(sample);
+    return;
+  };
   //husk actions vi har fundet, indenfor 5 sekunder, så vi kan tjekke fejl der går på tværs af samples
-  private Cache<String, Action> receivedActions = CacheBuilder
+  private Cache<String, Sample> uncleanSamples = CacheBuilder
       .newBuilder()
       .expireAfterWrite(5, TimeUnit.SECONDS)
+      .removalListener(sanitizerListener)
       .build();
   private Reasoner reasoner = Reasoner.getInstance();
 
@@ -61,9 +77,11 @@ public class Sampler {
    */
   public Sample getSample(NormalizedSensorState newState) {
     List<Action> acs = findActions(mPrevious,newState);
-    findInvertedActionsAndCleanStates(acs, mPrevious, newState);
+    findInvertedActionsAndCleanStates(new Sample(mHistory,newState.getTime().getNano(),acs), newState); //important: The sample here is not the same as the one below, as newState is modified in this method
+    List<Action> acsCorrected = findActions(mPrevious, newState); //we find actions again, as newState is modified
     moveScope(newState);
-    return new Sample(mHistory,newState.getTime().getNano(),acs);
+    Sample res = new Sample(mHistory, newState.getTime().getNano(), acsCorrected);
+    return res;
   }
 
   private void moveScope(NormalizedSensorState newState){
@@ -104,16 +122,24 @@ public class Sampler {
     return new Action(action.getVal2(), action.getVal1(), action.getDevice());
   }
 
-  private void findInvertedActionsAndCleanStates(List<Action> actions, NormalizedSensorState state1, NormalizedSensorState state2){
-    //region Update found actions cache
-    for (Action action: actions){
-      receivedActions.put(action.toString(), action);
-    }
+  /**
+   * Cleans states, by finding actions which are inverted, and resetting their values to the correct value
+   * @param sample the current sample
+   * @param state2 the state which correlates to the action
+   */
+  private void findInvertedActionsAndCleanStates(Sample sample, NormalizedSensorState state2){
+    //region Update found samples cache
+    uncleanSamples.put(sample.toString(), sample);
     //endregion
 
     //region Identify and correct inverted actions
-    receivedActions.cleanUp();
-    List<Action> validActions = new ArrayList<Action>(receivedActions.asMap().values());
+    List<Action> validActions = new ArrayList<Action>();
+    for (Sample s: uncleanSamples.asMap().values())
+    {
+      validActions.addAll(s.getActions());
+    }
+
+
     for (int i = 0; i < validActions.size() - 1; i++) {
       for (int j = i + 1; j < validActions.size() - 1; j++) { //Get all combinations of actions in sample
         if (validActions.get(i) == inverseAction(validActions.get(j))) { //If two actions are inverse to each other
@@ -122,13 +148,23 @@ public class Sampler {
               .get(state2
                   .getNormalizesValues()
                   .indexOf(validActions
-                      .get(j)
+                      .get(i)
                       .getVal2()))
               .setValue(validActions
                   .get(j)
+                  .getVal2()
+                  .getValue()); //Correct the state to have the value of before the action happened
+          state2
+              .getNormalizesValues()
+              .get(state2
+                  .getNormalizesValues()
+                  .indexOf(validActions
+                      .get(j)
+                      .getVal1()))
+              .setValue(validActions
+                  .get(i)
                   .getVal1()
                   .getValue()); //Correct the state to have the value of before the action happened
-
           if(reasoner.wasSystemAction(validActions.get(i))){
             //Flag the model in the DB
             //Tell Reasoner to update its model??
