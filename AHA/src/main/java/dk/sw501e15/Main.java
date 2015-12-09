cd09462848d5dccd49040f25fa5ee9baef7ae294
Krokcy <rasmus.chr11@gmail.com>
2015-12-09 11:36:21 +0100

    Main got fucked, reverted to commit: f61e355
    Minor spelling in T&V

--- AHA/src/main/java/dk/sw501e15/Main.java
+++ AHA/src/main/java/dk/sw501e15/Main.java
@@ -5,11 +5,16 @@ import Communication.DataReceiver;
 import Communication.SensorPacketWorker;
 import Communication.SensorState;
 import Database.DB;
+import Learner.Learner;
 import Normaliser.NormalizedSensorState;
 import Normaliser.NormalizedValue;
 import Normaliser.Normalizer;
+import Reasoner.Reasoner;
 import Sampler.*;
 
+import java.sql.Time;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedTransferQueue;
@@ -23,21 +28,57 @@ public class Main
     SensorPacketWorker oWorker = new SensorPacketWorker();
     DataReceiver dr = new DataReceiver(oWorker);
 
-        int scopeSize = 6;
-        int emulatableNum = 2;
+    Communicator oCommunicator = new Communicator("COM6", 9600, dr);
+    Normalizer nm = Normalizer.getInstance();
+    Queue<SensorState> queueOfSensorState = new LinkedTransferQueue<SensorState>();
+    oWorker.registerOutputTo(queueOfSensorState);
 
     Sample sample;
     DB db = DB.getInstance();
 
-        List<NormalizedValue> nValueList;
-        NormalizedSensorState nState;
+    db.createDB();
+    Sampler sampler = Sampler.getInstance(0,0);
 
-        while (true)
+    Reasoner oReasoner = Reasoner.getInstance();
+    oReasoner.setCommunicator(oCommunicator);
+
+    NormalizedSensorState nState;
+
+    Instant learnerRun = Instant.now();
+    long learnerRunInverval = 60; //in seconds
+    boolean learnerHasRun = false;
+
+    Thread learnerThread = new Thread(){
+      public synchronized void run(){
+        while(true){
+          System.out.println("Ran learner");
+          DB db = DB.getInstance();
+          Learner oLearner = new Learner();
+          //db.pushModel(oLearner.learn(db.getHistory())); //get the history to learn on and push the model once finished
+          try{
+            this.wait();
+          } catch(InterruptedException e) {
+
+          }
+        }
+      }
+    };
+    Learner oLearner = new Learner();
+    List<Sample> learnerData = new ArrayList<>();
+    while (true)
+    {
+      while (!queueOfSensorState.isEmpty())
+      {
+        SensorState oST = queueOfSensorState.poll();
+        nState = nm.Normalize(oST);
+        if(queueOfSensorState.size() > 10) {
+          System.out.println("Size of queue: " + queueOfSensorState.size());
+        }
+        if (nState != null)
         {
           sample = sampler.getSample(nState);
           oReasoner.reason(sample);
           db.putStateScopeIntoDB(sample);
-
 
         }
       }
@@ -53,4 +94,5 @@ public class Main
         }
       }
     }
+  }
 }
